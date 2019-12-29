73a32d10b011bc9f5f7297de7cc48b78df23ea17
Tan Nguyen <tannguyen12.07.sk@gmail.com>
2019-12-30 00:23:09 +0700

    [++]-[connect redux - fetch questions - get answers]

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,57 +1,46 @@
-import React, { useState } from 'react';
-import QuestionOption from 'components/QuestionOption';
+import React from 'react';
+import Option from 'components/Option';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Question = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(1);
-  const dataOption = [1, 2, 3, 4];
+const Question = props => {
+  const { indexQuestion, name, options, disabled } = props;
+  const { answers } = useSelector(state => state);
 
-  const onPrevQuestion = e => {
-    if (currentQuestion === 1) e.preventDefault();
-    else setCurrentQuestion(currentQuestion + Number(e.target.dataset.set));
-  };
-  const onNextQuestion = e => {
-    if (currentQuestion === 10) e.preventDefault();
-    else setCurrentQuestion(currentQuestion + Number(e.target.dataset.set));
-  };
+  const selectedOption = answers[indexQuestion]
+    ? Number(answers[indexQuestion].id)
+    : 0;
 
-  const listOption = dataOption.map((item, index) => (
-    <QuestionOption key={item} index={index} />
+  const listOption = options.map((option, index) => (
+    <Option
+      key={option.id}
+      index={index}
+      name={option.name}
+      id={option.id}
+      indexAnswer={indexQuestion}
+      isAnswer={option.isAnswer}
+      selected={selectedOption}
+      disabled={disabled}
+    />
   ));
   return (
-    <div>
-      <div className="app__title">
-        <h1> QUIZ APP </h1>
-      </div>
+    <>
       <div className="container">
-        <h2 className="app__heading"> Question {currentQuestion}: </h2>
+        <h2 className="app__heading app__heading--italic">
+          Question {indexQuestion + 1}:
+        </h2>
+        <div className="app__question">{name}</div>
         <div className="app-answer">{listOption}</div>
       </div>
-      <div className="app__control">
-        <div>
-          <button
-            data-set={-1}
-            className={
-              currentQuestion < 2 ? 'app__link app__link--disable' : 'app__link'
-            }
-            type="button"
-            onClick={onPrevQuestion}>
-            Prev
-          </button>
-        </div>
-        <div>
-          <button
-            data-set={1}
-            className={
-              currentQuestion > 9 ? 'app__link app__link--disable' : 'app__link'
-            }
-            type="button"
-            onClick={onNextQuestion}>
-            Next
-          </button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
-export default Question;
+Question.propTypes = {
+  indexQuestion: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  options: PropTypes.instanceOf(Array).isRequired,
+};
+
+export default React.memo(Question);
